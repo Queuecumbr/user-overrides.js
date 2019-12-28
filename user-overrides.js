8b0399211c2563754d4f240484b5c3a9be571707
Queuecumbr <11492746+Queuecumbr@users.noreply.github.com>
2019-12-28 22:16:09 +0000

    Update user-overrides.js

--- user-overrides.js
+++ user-overrides.js
@@ -1,3 +1,4 @@
+// QOL Overrides
 user_pref("app.update.auto", true);
 user_pref("privacy.clearOnShutdown.history", false);
 user_pref("privacy.clearOnShutdown.cookies", false);
@@ -11,3 +12,43 @@ user_pref("browser.startup.page", 1);
 user_pref("browser.startup.homepage", "about:home");
 user_pref("app.update.auto", true);
 user_pref("browser.shell.shortcutFavicons", true);
+
+// Enable WebGl
+user_pref("webgl.disabled", false);
+user_pref("webgl.enable-webgl2", true);
+user_pref("webgl.min_capability_mode", false);
+user_pref("webgl.disable-extensions", false);
+
+// Enable cache
+user_pref("browser.cache.disk.enable", true);
+
+// Search with urlbar when not inputing a web address
+user_pref("keyword.enabled", true);
+
+// Disable safebrowsing
+user_pref("browser.safebrowsing.malware.enabled", false);
+user_pref("browser.safebrowsing.phishing.enabled", false);
+user_pref("browser.safebrowsing.downloads.enabled", false);
+user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false);
+user_pref("browser.safebrowsing.downloads.remote.block_uncommon", false);
+
+// Personal privacy settings
+user_pref("extensions.screenshots.upload-disabled", true);
+user_pref("browser.urlbar.suggest.history", false);
+user_pref("signon.rememberSignons", false);
+user_pref("media.autoplay.default", 5);
+user_pref("dom.webnotifications.enabled", false);
+user_pref("dom.webnotifications.serviceworker.enabled", false);
+user_pref("permissions.default.desktop-notification", 2);
+user_pref("dom.battery.enabled", false);
+user_pref("dom.vr.enabled", false);
+user_pref("browser.download.folderList", 2);
+user_pref("network.cookie.lifetimePolicy", 2);
+user_pref("dom.w3c_touch_events.enabled", 0);
+
+// Personal QOL Tweaks
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.backspace_action", 2);
+user_pref("extensions.pocket.enabled", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
